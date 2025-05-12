59555ed521a0a22f0c3e267b0bd9cc9b2b7db0ef
abhisarxverma <absrshanks@gmail.com>
2025-05-12 22:37:44 +0530

    Linked List Node implementation to use everywhere in problems

---
+++ Linked_List/Linked_List.h
@@ -0,0 +1,55 @@
+#ifndef LINKED_LIST_H
+#define LINKED_LIST_H
+
+#include <iostream>
+using namespace std;
+
+class Node {
+
+    public:
+    int data;
+    Node *next;
+
+    public:
+    Node(int data1, Node *next1){
+        data = data1;
+        next = next1;
+    }
+
+    public:
+    Node(int data1){
+        data = data1;
+        next = nullptr;
+    }
+};
+
+void traverse_linked_list( Node *head ){
+    if (!head) {
+        cout << "Linked List is empty!" << endl;
+        return;
+    }
+    Node *mover = head;
+    while (mover) {
+        cout << mover -> data;
+        if (mover -> next) cout << " -> ";
+        else cout << endl;
+        mover = mover -> next;
+    }
+}
+
+// Returns already created example linked list
+Node *example_linked_list() {
+    
+    // Example Linked list
+    Node *head = new Node(10);
+    head -> next = new Node(15);
+    head -> next -> next = new Node(20);
+    head -> next -> next -> next = new Node(25);
+    head -> next -> next -> next -> next = new Node(30);
+    head -> next -> next -> next -> next -> next = new Node(35);
+    head -> next -> next -> next -> next -> next -> next = new Node(40);
+
+    return head;
+}
+
+#endif
---
+++ Linked_List/Linked_List.py
@@ -0,0 +1,25 @@
+
+class Node:
+    def __init__(self, data, next=None):
+        self.data = data
+        self.next = next
+
+def tarverse_linked_list( head: Node ):
+    """Traverse and print the data of the linked list."""
+    if not head: print("Linked List is Empty.")
+    else:
+        mover = head
+        while mover:
+            print(mover.data, end=" -> " if mover.next else "\n")
+            mover = mover.next
+
+def example_linked_list() -> Node :
+    head = Node(10)
+    head.next = Node(15)
+    head.next.next = Node(20)
+    head.next.next.next = Node(25)
+    head.next.next.next.next = Node(30)
+    head.next.next.next.next.next = Node(35)
+    head.next.next.next.next.next.next = Node(40)
+
+    return head
