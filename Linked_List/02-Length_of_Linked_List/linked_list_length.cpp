936522da9fb5b2c65a82c62e0d981a14ac1f5e9c
abhisarxverma <absrshanks@gmail.com>
2025-05-13 16:15:26 +0530

    Linked List : problem 2 : Given a linked list return the length of the linked list

--- Linked_List/02-Length_of_Linked_List/linked_list_length.cpp
+++ Linked_List/02-Length_of_Linked_List/linked_list_length.cpp
@@ -2,12 +2,11 @@
 #include <iostream>
 using namespace std;
 
-// PROBLEM - Given head (Node object) of Linked List, return the length of the linked list.
-
 /*
+PROBLEM - Given head (Node object) of Linked List, return the length of the linked list.
+
 APPROACH
 ---------
-
 LOGIC - Traverse the Linked List and calculate the length
 Take a counter variable "length"
 Take a mover pointer variable, move the mover to it's next till the mover becomes None and increment the count ever time.
--- Linked_List/02-Length_of_Linked_List/linked_list_length.py
+++ Linked_List/02-Length_of_Linked_List/linked_list_length.py
@@ -13,12 +13,12 @@ def tarverse_linked_list( head: Node ):
             print(mover.data, end=" -> " if mover.next else "\n")
             mover = mover.next
 
-# PROBLEM - Given head (Node object) of Linked List, return the length of the linked list.
 
 '''
+PROBLEM - Given head (Node object) of Linked List, return the length of the linked list.
+
 APPROACH
 ---------
-
 LOGIC - Traverse the Linked List and calculate the length
 Take a counter variable "length"
 Take a mover pointer variable, move the mover to it's next till the mover becomes None and increment the count ever time.
